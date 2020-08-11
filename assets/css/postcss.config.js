5ea59ccd2824611388ea6e42aac0ecd2f8767192
Sathish <sathish.babu.krishna.vihar@sap.com>
2020-08-11 11:56:22 -0700

    consumption changes
    * [x] remove separate purgecss
    * [x] remove opacity-75

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,7 +1,5 @@
 const themeDir = __dirname + '/../../';
 
-const purgecss = require('purgecss')
-
 module.exports = {    
     plugins: [        
         require('postcss-import')({
@@ -10,29 +8,6 @@ module.exports = {
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
         require('autoprefixer')({
             path: [themeDir]
-        }),
-        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss({
-
-            // Specify the paths to all of the template files in your project
-            content: [
-                themeDir + 'layouts/**/*.html',
-                themeDir + 'content/**/*.html',
-                'layouts/**/*.html',
-                'content/**/*.html',
-                'exampleSite/layouts/**/*.html',
-                'exampleSite/content/**/*.html',
-            ],
-        
-            // This is the function used to extract class names from your templates
-            defaultExtractor: content => {
-                // Capture as liberally as possible, including things like `h-(screen-1.5)`
-                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-        
-                // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-        
-                return broadMatches.concat(innerMatches)
-            }
-        })] : [])
+        })
     ]
 }
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: [
+    '**/*.html'
+  ],
   theme: {
     extend: {},
   },
